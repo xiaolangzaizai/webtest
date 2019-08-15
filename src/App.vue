1879621d734777fbc29527209722d21bed8f7361
xiaolang <2399308826@qq.com>
2019-08-15 16:32:10 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+	<div id="app">
+		<div class="container">
+			<nav class="navbar navbar-default role=" navigation ">
+				<div></div>
+  			<div class="container-fluid ">
+  				<div class="navbar-header ">
+  					<a class="navbar-brand active " href="# ">首页</a>
+  				</div>
+  				<div>
+  					<ul class="nav navbar-nav ">
+	  					<li>
+	  						<router-link to="/Empadd ">雇员新增</router-link>
+	  					</li>
+	  					<li>
+	  						
+	  						<router-link to="/Empadd ">雇员列表</router-link>
+	  					</li>
+  					</ul>
+  				</div>
+  			</div>
+  		</nav>
+  		    <router-view/>
+
+  	</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+</style>
---
+++ src/EmpUpload.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <!--<form action="http://localhost:8081/upload" method="post" 
+    	enctype="multipart/form-data">
+    	<input type="file"  name="file"/>
+    	<input type="submit" class="btn btn-default" value="上传" />
+    </form>-->
+    <input type="text" id="username" name="username" />
+    <input type="file"  name="file"/>
+    <input type="button" class="btn btn-default" v-on:click="upload" value="上传" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmpUpload',
+  methods:{
+  	upload:function(){
+  		var formdata = new FormData();
+  		formdata.append("file",document.querySelector("input[name=file]").files[0]);
+  		formdata.append("username",document.getElementById("username").value);
+  		
+  		var config={
+  			headers:{
+  				"Content-type":"multipart/form-data"
+  			}
+  		};
+  		this.$http.post("http://localhost:8081/upload",formdata,config).then(
+  			function(result){
+  				alert(result.bodyText);
+  			},
+  			function(error){
+  				alert(error.body.message);
+  			}
+  		)
+  		
+  	}
+  }
+  
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
